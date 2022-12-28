9843b9780e4083f7804cccf90882a43ac6397f47
markus <datamastery87@gmail.com>
2022-12-28 16:03:43 +0100

    fix code

--- 00.Basics/async_functions.py
+++ 00.Basics/async_functions.py
@@ -3,13 +3,34 @@ import asyncio
 
 ones = [1] * 10
 
+
 def blocking_task(n: int) -> int:
     time.sleep(n)
     return n
 
+
 async def non_blocking_task(n: int) -> int:
     await asyncio.sleep(n)
     return n
+
+
+start_time = time.perf_counter()
+blocking_task(5)
+end_time = time.perf_counter()
+
+print(f"Blocking task completed in {end_time - start_time:.2f} seconds")
+
+
+async def main():
+    start_time = time.perf_counter()
+    await non_blocking_task(5)
+    end_time = time.perf_counter()
+
+    print(f"Non-blocking task completed in {end_time - start_time:.2f} seconds")
+
+
+asyncio.run(main())
+
 
 def blocking_function():
     results = []
@@ -17,12 +38,14 @@ def blocking_function():
         results.append(blocking_task(n))
     return results
 
+
 async def non_blocking_function():
     results = []
     for n in ones:
         task = asyncio.create_task(non_blocking_task(n))
         results.append(task)
     return await asyncio.gather(*results)
+
 
 start = time.perf_counter()
 print(blocking_function())
